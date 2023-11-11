aa17032fcecb19dcd5aa522adb6685a9387d5430
tigeregg <thetigeregg@gmail.com>
2023-11-11 18:30:32 -0500

    Add game platform to completion text

--- utils/prompt.py
+++ utils/prompt.py
@@ -5,16 +5,24 @@ from prompt_toolkit.completion import Completer, Completion
 class GameNameCompleter(Completer):
     def get_completions(self, document, complete_event):
         word = document.text_before_cursor.lower()
+        game_name_suggestions = get_game_name_suggestions(self.games_dict)
         suggestions = process.extract(
-            word, get_game_name_suggestions(self.games_dict), limit=10
+            word, [game[0] for game in game_name_suggestions], limit=10
         )
         for suggestion, score in suggestions:
             if score > 70:  # adjust the score as needed
-                yield Completion(suggestion, start_position=-len(word))
+                for game_name, game_name_with_platform in game_name_suggestions:
+                    if game_name == suggestion:
+                        yield Completion(
+                            game_name_with_platform, start_position=-len(word)
+                        )
 
     def __init__(self, games_dict):
         self.games_dict = games_dict
 
 
 def get_game_name_suggestions(games_dict):
-    return list(games_dict.keys())
+    return [
+        (game["name"], game["name"] + " (" + game["platform"] + ")")
+        for game in games_dict.values()
+    ]
