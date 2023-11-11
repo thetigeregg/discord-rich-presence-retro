df311a277b389473df78e6e196042688b7785cd7
tigeregg <thetigeregg@gmail.com>
2023-11-11 18:22:23 -0500

    Use fuzzy matching

--- utils/prompt.py
+++ utils/prompt.py
@@ -1,12 +1,16 @@
+from fuzzywuzzy import process
 from prompt_toolkit.completion import Completer, Completion
 
 
 class GameNameCompleter(Completer):
     def get_completions(self, document, complete_event):
         word = document.text_before_cursor.lower()
-        for game_name in get_game_name_suggestions(self.games_dict):
-            if word in game_name:
-                yield Completion(game_name, start_position=-len(word))
+        suggestions = process.extract(
+            word, get_game_name_suggestions(self.games_dict), limit=10
+        )
+        for suggestion, score in suggestions:
+            if score > 70:  # adjust the score as needed
+                yield Completion(suggestion, start_position=-len(word))
 
     def __init__(self, games_dict):
         self.games_dict = games_dict
