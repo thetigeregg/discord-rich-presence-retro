2c072a9484b6f851da640887fe10a6760d8d73ce
tigeregg <thetigeregg@gmail.com>
2023-11-11 16:42:04 -0500

    Format parse.py

--- utils/parse.py
+++ utils/parse.py
@@ -8,9 +8,7 @@ def load_games_list():
     games_dict = {}
     with open(CSV_FILE_PATH, "r") as file:
         reader = csv.DictReader(file)
-        for i, row in enumerate(reader):
-            if i >= 5:
-                break
+        for row in reader:
             normalized_name = normalize_game_name(row["name"])
             games_dict[normalized_name] = row
     return games_dict
